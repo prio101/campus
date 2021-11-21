4080c57a184c4d5bc15ac00c2c2f5fa5cc3db1ec
Mahabub Islam Prio <miprio101@gmail.com>
2021-11-21 11:41:55 +0600

    even odd: added two ways of the even odd

--- algorithm/even_odd.py
+++ algorithm/even_odd.py
@@ -7,14 +7,22 @@ def take_input():
 
 def even_odd(str):
   length = len(str)
-
   last_index = length - 1
-
   
+  # modules Way:
+  print("modulas way:")
   if int(str[last_index]) % 2 == 1:
     print("it is odd")
   else:
     print("it is even")
+
+  # bitwise way:
+  print("bitwise way:")
+  if int(str[last_index])&1 == 1:
+    print("it is odd")
+  else:
+    print("it is even")
+
 
 def main():
   str = take_input()
